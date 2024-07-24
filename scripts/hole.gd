08db7ec533e2f134516a4ddc98fcaa7fb082ae3a
Éster Augusto <61546216+gutoFaria@users.noreply.github.com>
2024-07-24 10:24:22 -0300

    mini

---
+++ scripts/hole.gd
@@ -0,0 +1,92 @@
+extends Node3D
+
+
+enum {AIM,SET_POWER,SHOOT,WIN}
+
+@export var power_speed = 100
+@export var angle_speed = 1.1
+@export var mouse_sensitivity = 150
+
+var angle_change = 1
+var power = 0
+var power_change = 1
+var shots = 0
+var state = AIM
+var hole_dir
+
+func _ready():
+	$Arrow.hide()
+	$Ball.position = $Tee.position
+	change_state(AIM)
+	$UI.show_message("Get Ready!")
+
+func change_state(new_state):
+	state = new_state
+	match state:
+		AIM:
+			$Arrow.position = $Ball.position
+			$Arrow.show()
+			set_start_angle()
+		SET_POWER:
+			power = 0
+		SHOOT:
+			$Arrow.hide()
+			$Ball.shoot($Arrow.rotation.y, power / 15)
+			shots += 1
+			$UI.update_shots(shots)
+		WIN:
+			$Ball.hide()
+			$Arrow.hide()
+			$UI.show_message("Win!")
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		if state == AIM:
+			$Arrow.rotation.y -= event.relative.x / mouse_sensitivity
+			
+	if event.is_action_pressed("click"):
+		match state:
+			AIM:
+				change_state(SET_POWER)
+			SET_POWER:
+				change_state(SHOOT)
+
+func _process(delta):
+	match state:
+		AIM:
+			animate_arrow(delta)
+		SET_POWER:
+			animate_power(delta)
+		SHOOT:
+			pass
+
+func animate_arrow(delta):
+	$Arrow.rotation.y += angle_speed * angle_change * delta
+	if $Arrow.rotation.y > hole_dir + PI / 2:
+		angle_change = -1
+	if $Arrow.rotation.y < hole_dir - PI / 2:
+		angle_change = 1
+
+func animate_power(delta):
+	power += power_speed * power_change * delta
+	if power >= 100:
+		power_change = -1
+	if power <= 0:
+		power_change = 1
+	$UI.update_power_bar(power)
+
+func set_start_angle():
+	var hole_position = Vector2($Hole.position.z, $Hole.position.x)
+	var ball_position = Vector2($Ball.position.z,$Ball.position.x)
+	hole_dir = (ball_position - hole_position).angle()
+	$Arrow.rotation.y = hole_dir
+	
+func _on_hole_body_entered(body):
+	if body.name == "Ball":
+		print("win!")
+		change_state(WIN)
+
+
+func _on_ball_stopped():
+	if state == SHOOT:
+		change_state(AIM)
